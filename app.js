4afc9341b03efc44e85bbf598433c71b9a6cebab
akhilch25 <akhilreddych.25@gmail.com>
2024-10-07 11:01:34 +0530

    Updated

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use('/app', employeeCourseRoutes);
 app.use('/app',courseRoutes);
 app.use('/app',employeeRoutes)
 
-const PORT=process.env.PORT || 5000;
+const PORT= 5000;
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 })
